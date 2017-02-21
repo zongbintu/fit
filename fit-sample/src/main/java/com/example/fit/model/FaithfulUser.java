e3ed68715a0eac8f4f244d0b061427ed996aa323
2Tu <enum@foxmail.com>
2017-02-21 22:15:55 +0800

    support getter/setter, fixed #17,#5

--- fit-sample/src/main/java/com/example/fit/model/FaithfulUser.java
+++ fit-sample/src/main/java/com/example/fit/model/FaithfulUser.java
@@ -12,6 +12,73 @@ import fit.SharedPreferenceAble;
 
   public long joinTime;
 
+  protected String a;
+
+  public long tlong;
+
+  private String n;
+  private boolean smaller;
+  private Integer age;
+  private int _role;
+
+  String b;
+
+  public String getN() {
+    return n;
+  }
+
+  public void setN(String n) {
+    this.n = n;
+  }
+
+  public long getJoinTime() {
+    return joinTime;
+  }
+
+  public void setJoinTime(long joinTime) {
+    this.joinTime = joinTime;
+  }
+
+  public String getA() {
+    return a;
+  }
+
+  public void setA(String a) {
+    this.a = a;
+  }
+
+  public boolean isSmaller() {
+    return smaller;
+  }
+
+  public void setSmaller(boolean smaller) {
+    this.smaller = smaller;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public int get_role() {
+    return _role;
+  }
+
+  public void set_role(int _role) {
+    this._role = _role;
+  }
+
+  public String getB() {
+    return b;
+  }
+
+  public void setB(String b) {
+    this.b = b;
+  }
+
   class Inner {
     public char cInner;
   }
