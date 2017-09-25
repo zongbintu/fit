5f583212ff0837b2f11f9d6542dd14e5a2c2407d
2Tu <enum@foxmail.com>
2017-09-25 18:56:03 +0800

    use Object save, Enum\Array\Serializable

--- fit-sample/src/main/java/com/example/fit/model/User.java
+++ fit-sample/src/main/java/com/example/fit/model/User.java
@@ -2,6 +2,7 @@ package com.example.fit.model;
 
 import fit.SharedPreferenceAble;
 import java.util.HashSet;
+import java.util.List;
 
 /**
  * @auther Tu enum@foxmail.com
@@ -34,30 +35,228 @@ import java.util.HashSet;
   public Boolean aBoolean;
   public Character character;
 
+  public List<Hobby> hobbies;
+  private Job job;
+
+  private transient Job transientJob;
+
   public User() {
 
   }
 
+  public Job getTransientJob() {
+    return transientJob;
+  }
+
+  public void setTransientJob(Job transientJob) {
+    this.transientJob = transientJob;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public HashSet<String> getHobby() {
+    return hobby;
+  }
+
+  public void setHobby(HashSet<String> hobby) {
+    this.hobby = hobby;
+  }
+
+  public byte getTbyte() {
+    return tbyte;
+  }
+
+  public void setTbyte(byte tbyte) {
+    this.tbyte = tbyte;
+  }
+
+  public short getTshort() {
+    return tshort;
+  }
+
+  public void setTshort(short tshort) {
+    this.tshort = tshort;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public long getTlong() {
+    return tlong;
+  }
+
+  public void setTlong(long tlong) {
+    this.tlong = tlong;
+  }
+
+  public float getTfloat() {
+    return tfloat;
+  }
+
+  public void setTfloat(float tfloat) {
+    this.tfloat = tfloat;
+  }
+
+  public double getScore() {
+    return score;
+  }
+
+  public void setScore(double score) {
+    this.score = score;
+  }
+
+  public boolean isTboolean() {
+    return tboolean;
+  }
+
+  public void setTboolean(boolean tboolean) {
+    this.tboolean = tboolean;
+  }
+
+  public char getC() {
+    return c;
+  }
+
+  public void setC(char c) {
+    this.c = c;
+  }
+
+  public Byte getSex() {
+    return sex;
+  }
+
+  public void setSex(Byte sex) {
+    this.sex = sex;
+  }
+
+  public Short getS() {
+    return s;
+  }
+
+  public void setS(Short s) {
+    this.s = s;
+  }
+
+  public Integer getInteger() {
+    return integer;
+  }
+
+  public void setInteger(Integer integer) {
+    this.integer = integer;
+  }
+
+  public Long getaLong() {
+    return aLong;
+  }
+
+  public void setaLong(Long aLong) {
+    this.aLong = aLong;
+  }
+
+  public Float getaFloat() {
+    return aFloat;
+  }
+
+  public void setaFloat(Float aFloat) {
+    this.aFloat = aFloat;
+  }
+
+  public Double getaDouble() {
+    return aDouble;
+  }
+
+  public void setaDouble(Double aDouble) {
+    this.aDouble = aDouble;
+  }
+
+  public Boolean getaBoolean() {
+    return aBoolean;
+  }
+
+  public void setaBoolean(Boolean aBoolean) {
+    this.aBoolean = aBoolean;
+  }
+
+  public Character getCharacter() {
+    return character;
+  }
+
+  public void setCharacter(Character character) {
+    this.character = character;
+  }
+
+  public List<Hobby> getHobbies() {
+    return hobbies;
+  }
+
+  public void setHobbies(List<Hobby> hobbies) {
+    this.hobbies = hobbies;
+  }
+
+  public Job getJob() {
+    return job;
+  }
+
+  public void setJob(Job job) {
+    this.job = job;
+  }
+
   @Override public String toString() {
-    return "User{" +
-        "name='" + name + '\'' +
-        ", hobby=" + hobby +
-        ", tbyte=" + tbyte +
-        ", tshort=" + tshort +
-        ", age=" + age +
-        ", tlong=" + tlong +
-        ", tfloat=" + tfloat +
-        ", score=" + score +
-        ", tboolean=" + tboolean +
-        ", c=" + c +
-        ", sex=" + sex +
-        ", s=" + s +
-        ", integer=" + integer +
-        ", aLong=" + aLong +
-        ", aFloat=" + aFloat +
-        ", aDouble=" + aDouble +
-        ", aBoolean=" + aBoolean +
-        ", character=" + character +
-        '}';
+    return "User{"
+        + "name='"
+        + name
+        + '\''
+        + ", hobby="
+        + hobby
+        + ", tbyte="
+        + tbyte
+        + ", tshort="
+        + tshort
+        + ", age="
+        + age
+        + ", tlong="
+        + tlong
+        + ", tfloat="
+        + tfloat
+        + ", score="
+        + score
+        + ", tboolean="
+        + tboolean
+        + ", c="
+        + c
+        + ", sex="
+        + sex
+        + ", s="
+        + s
+        + ", integer="
+        + integer
+        + ", aLong="
+        + aLong
+        + ", aFloat="
+        + aFloat
+        + ", aDouble="
+        + aDouble
+        + ", aBoolean="
+        + aBoolean
+        + ", character="
+        + character
+        + ", hobbies="
+        + hobbies
+        + ", job="
+        + job
+        + ", transientJob="
+        + transientJob
+        + '}';
   }
 }
---
+++ fit/src/main/java/fit/internal/FileObjectUtil.java
@@ -0,0 +1,70 @@
+package fit.internal;
+
+import android.content.Context;
+import android.util.Log;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import static fit.Fit.TAG;
+
+/**
+ * @author Tu enum@foxmail.com.
+ * @since 0.4.0
+ */
+
+public final class FileObjectUtil {
+  /**
+   * write object to file
+   *
+   * @param context {@link Context}
+   * @param fileName file name
+   * @param obj Type is String/Array/Serializable
+   */
+  public static void writeObject(Context context, String fileName, Object obj) {
+    FileOutputStream fileOutputStream = null;
+    ObjectOutputStream objectOutputStream = null;
+    try {
+      fileOutputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE);
+      objectOutputStream = new ObjectOutputStream(fileOutputStream);
+      objectOutputStream.writeObject(obj);
+    } catch (FileNotFoundException e) {
+      Log.e(TAG, "FileOutputStream error", e);
+    } catch (IOException e) {
+      Log.e(TAG, "ObjectOutputStream error", e);
+    } finally {
+      Utils.closeQuietly(fileOutputStream);
+      Utils.closeQuietly(objectOutputStream);
+    }
+  }
+
+  public static Object readObject(Context context, String fileName) {
+    ObjectInputStream objectInputStream = null;
+    FileInputStream fileInputStream = null;
+    try {
+      fileInputStream = context.openFileInput(fileName);
+      objectInputStream = new ObjectInputStream(fileInputStream);
+      Object obj = objectInputStream.readObject();
+      return obj;
+    } catch (IOException e) {
+      Log.e(TAG, "ObjectInputStream error", e);
+    } catch (ClassNotFoundException e) {
+      Log.e(TAG, "FileInputStream error", e);
+    } finally {
+      Utils.closeQuietly(objectInputStream);
+      Utils.closeQuietly(fileInputStream);
+    }
+    return null;
+  }
+
+  public static <T> T readObject(Context context, String fileName, Class<T> clazz) {
+    Object obj = readObject(context, fileName);
+    if (null != obj) {
+      return clazz.cast(obj);
+    }
+    return null;
+  }
+}
